55b840c1d494af4fee99f1f5e044c55054e34bdb
Victor <viclee02@gmail.com>
2018-03-03 18:57:04 -1000

    complete ex2

--- app.py
+++ app.py
@@ -17,3 +17,30 @@ print('b: ' + str(b))
 
 # Warning! Do not use the programming language magic. After you complete the exercise feel free to do so.
 
+# ex.2
+# It's the end of the semester and you got your marks from, Geometry, Algebra, Physics classes.
+
+# Create a program that:
+# * Reads the values of these 3 lessons
+# * Calculates the average of your grades
+# * Example: Geometry = 6, Algebra = 7, Physics = 8
+# * Output: average_score = 7
+
+# Warning! Do not use the programming language magic. After you complete the exercise feel free to do so.
+# 
+grades = {'geometry': 6, 'algegra': 7, 'physics': 8}
+print(grades) #geometry', 'physics', 'algegra'}
+print(grades['geometry']) #6
+
+print(list(grades)) #['geometry', 'physics', 'algegra']
+print(grades.keys())#['geometry', 'physics', 'algegra']
+print(grades.values())#[6, 8, 7]
+print(sum(grades.values()) / len(grades))#7
+average_score = sum(grades.values()) / len(grades)
+print(average_score) #7
+
+
+
+
+
+
