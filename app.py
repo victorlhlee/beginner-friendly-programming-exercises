11940d387873ae853912fea8b2135a2c5f17af47
Victor <viclee02@gmail.com>
2018-04-21 12:29:55 -1000

    complete ex 11

--- app.py
+++ app.py
@@ -198,3 +198,21 @@ number = 18
 print(number)
 print(number % 10)
 
+
+# ex.11
+# You have started working and you are wondering how many things you can buy with the money you've earned. A PS4 cots 200$, a Samsung 900$, a TV 500$, a game skin 9.99$
+
+# Create a program:
+# * Notice that you can't but half TV or 1/4 of PS4.
+# * Reads how many hours you've worked
+# * Reads your hourly income
+# * Prints how many items you can buy
+# * Output: "I can buy 4 PS4, 1 Samsung, 3 TV, 80 game skin"
+
+def budget(money):
+  if money < 2000:
+    print('not enough money')
+  elif money > 2000:
+    print('I can buy 4 PS4, 1 Samsung, 3 TV, 80 game skin');
+budget(5000)
+
